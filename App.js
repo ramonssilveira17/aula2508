005b8e9de7714ba0914267f61bb2332ec3719353
ramonssilveira17 <swakegot@gmail.com>
2020-08-25 17:29:39 -0300

    using icons in app react-native-vector-icons

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+Icon.loadFont()
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.views, styles.child1]}></View>
+      <Icon name="android" size={50} color={"#783762"}></Icon>
+      <View style={[styles.views, styles.child2]}></View>
+      <View style={[styles.views, styles.child3]}></View>
     </View>
   );
 }
@@ -15,7 +19,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  views:{
+    width: 50,
+    height: 50,
+    margin: 10,
+  },
+  child1: {
+    backgroundColor: 'powderblue',
+    elevation: 10,
+    borderRadius: 5,
+  },
+  child2: {
+    backgroundColor: 'skyblue',
+    borderRadius: 15,
+  },
+  child3: {
+    backgroundColor:'steelblue',
+    borderRadius: 30,
+  },
+
 });
